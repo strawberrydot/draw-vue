e271c52a697df67e06c8743b5e2264318f56af08
guxuan <guxuan2012@hotmail.com>
2018-08-27 20:15:53 +0800

    2018-08-27commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import * as d3 from 'd3';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.d3 = d3;
+// 如果你需要应用一个插件，同事他并不是基于vue.js的插件命令编写的，那么你可以将它赋予Vue的原型上
+// 在使用的时候 this.XXX即可
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+});
---
+++ src/views/test/DrawTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="draw-test">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DrawTest",
+        mounted() {
+            let width = 900;
+            let height = 800;
+            let svg = this.d3.select('#draw-test').append('svg').attr('width', width).attr('height', height).attr('fill', '#0098FC');
+
+        }
+    };
+</script>
+
+<style>
+
+</style>
